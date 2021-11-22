b85579fc163f30ae1ea1688712d46c0d55e57661
Lukasz Zatyka <lukasz.zatyka@gmail.com>
2021-11-22 12:13:23 +0100

    Pie chart implement

--- visx-pie-shape/src/App.js
+++ visx-pie-shape/src/App.js
@@ -1,13 +1,82 @@
 import logo from './logo.svg';
 
+import { useState } from "react";
+import { Pie } from "@visx/shape";
+import { Group } from "@visx/group";
+import { Text } from "@visx/text";
+
+const coins = [
+  { symbol: "UMA", amount: 1000, color: "#0033ad", inUSD: 15.42 },
+  { symbol: "ETH", amount: 2, color: "#00ffbd", inUSD: 4210.96},
+  { symbol: "BTC", amount: 0.065, color: "#F7931A", inUSD: 57479.4 },
+];
+
 function App() {
+  const [active, setActive] = useState(null);
+  const width = 400;
+  const half = width / 2;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+      <main>
+        <svg width={width} height={width}>
+          <Group top={half} left={half}>
+            <Pie
+                data={coins}
+                pieValue={(data) => data.amount * data.inUSD}
+                outerRadius={half}
+                innerRadius={({data}) => {
+                  const size = active && active.symbol == data.symbol ? 12 : 8;
+                  return half - size;
+                }}
+                padAngle={0.01}
+            >
+              {(pie) => {
+                return pie.arcs.map((arc) => {
+                  return (
+                      <g
+                          key={arc.data.symbol}
+                          onMouseEnter={() => setActive(arc.data)}
+                          onMouseLeave={() => setActive(null)}
+                      >
+                        <path d={pie.path(arc)} fill={arc.data.color}></path>
+                      </g>
+                  );
+                });
+              }}
+            </Pie>
+            {active ? (
+                <>
+                  <Text textAnchor="middle" fill="#fff" fontSize={40} dy={-20}>
+                    {`$${Math.floor(active.amount * active.inUSD)}`}
+                  </Text>
+
+                  <Text
+                      textAnchor="middle"
+                      fill={active.color}
+                      fontSize={20}
+                      dy={20}
+                  >
+                    {`${active.amount} ${active.symbol}`}
+                  </Text>
+                </>
+            ) : (
+                <>
+                  <Text textAnchor="middle" fill="#fff" fontSize={40} dy={-20}>
+                    {`$${Math.floor(
+                        coins.reduce(
+                            (acc, coin) => acc + coin.amount * coin.inUSD, 0)
+                    )}`}
+                  </Text>
+
+                  <Text textAnchor="middle" fill="#aaa" fontSize={20} dy={20}>
+                    {`${coins.length} Assets`}
+                  </Text>
+                </>
+            )}
+          </Group>
+        </svg>
+      </main>
+  )
 }
 
 export default App;
